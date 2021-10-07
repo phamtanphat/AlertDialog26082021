5ce5775680f547089377ad72711b5b1c2b2dec29
phamtanphat <phatdroid94@gmail.com>
2021-10-07 20:14:01 +0700

    alert dialog

--- app/src/main/java/com/example/alertdialog26082021/MainActivity.java
+++ app/src/main/java/com/example/alertdialog26082021/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.alertdialog26082021;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mBtnAlertDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnAlertDialog = findViewById(R.id.buttonAlertDialog);
+
+        mBtnAlertDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
+                alertBuilder.setTitle("Thông báo");
+                alertBuilder.setMessage("Ứng có phiên bản mới. Câp nhật ngay bây giờ");
+                alertBuilder.setIcon(R.mipmap.ic_launcher);
+                alertBuilder.setCancelable(false);
+
+                alertBuilder.setPositiveButton("Đồng ý", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Đồng ý", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                alertBuilder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Không", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                alertBuilder.setNeutralButton("Hủy", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Hủy", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                alertBuilder.show();
+
+            }
+        });
     }
 }
